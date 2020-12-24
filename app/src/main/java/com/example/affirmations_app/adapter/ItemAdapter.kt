8146409e0580575d431d4ed2483fbcdecbc6eaac
Abdulrafay <abdulrafayn001@gmail.com>
2020-12-24 20:59:07 +0500

    Add Images to Recycler View

--- app/src/main/java/com/example/affirmations_app/adapter/ItemAdapter.kt
+++ app/src/main/java/com/example/affirmations_app/adapter/ItemAdapter.kt
@@ -3,29 +3,32 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations_app.R
 import com.example.affirmations_app.model.Affirmation
 
-class ItemAdapter(
-        private val context: Context, private val dataset : List<Affirmation>
+class ItemAdapter(private val context: Context, private val dataset : List<Affirmation>
         ):RecyclerView.Adapter<ItemAdapter.ItemViewHolder> (){
-
+    //nested class which holds the view
     class ItemViewHolder(private val view: View):RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
+        val imageView:ImageView=view.findViewById(R.id.item_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
         val adapterLayout = LayoutInflater.from(parent.context)
                 .inflate(R.layout.list_item, parent, false)
-
+        println("create")
         return ItemViewHolder(adapterLayout)
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        println("Bind")
         val item = dataset[position]
         holder.textView.text =  context.resources.getString(item.stringResourceId)
+        holder.imageView.setImageResource(item.imgResourceId)
     }
 
     override fun getItemCount(): Int {
--- app/src/main/java/com/example/affirmations_app/model/Affirmation.kt
+++ app/src/main/java/com/example/affirmations_app/model/Affirmation.kt
@@ -1,7 +1,8 @@
 package com.example.affirmations_app.model
 
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 
-data class Affirmation(@StringRes val stringResourceId:Int) {
+data class Affirmation(@StringRes val stringResourceId:Int,@DrawableRes val imgResourceId:Int) {
 
 }
